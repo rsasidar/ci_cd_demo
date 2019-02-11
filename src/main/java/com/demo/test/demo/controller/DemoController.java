1a3edd6410da9b417389f5e65a03deef4e86b0bd
Rejy Sasidharan <rejy.sasidharan@laerdal.com>
2019-02-11 11:05:10 +0530

    modified properties

---
+++ src/main/java/com/demo/test/demo/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.demo.test.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DemoController {
+	
+	@GetMapping("/messages")
+	public String retrieveCoursesForStudent() {
+		return "Test Message.....";
+	}
+	
+	@GetMapping("/greeting")
+	public String retrieveDetailsForCourse() {
+		return "Greetings .... ";
+	}
+
+}
